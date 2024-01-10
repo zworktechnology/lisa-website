e4bfa878e87e69455a5b8b12f0867dbb395e565c
Alaguraja <alaguraja.mscmba@gmail.com>
2024-01-10 18:05:07 +0530

    added terms and privacy page

---
+++ resources/views/privacy_policy.blade.php
@@ -0,0 +1,220 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Lisa | Privacy Policy</title>
+    <link rel="icon" href="images\Lisa Final Logo File_page-0001.jpg" type="image/x-icon">
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <style>
+        html {
+            overflow-x: hidden !important
+        }
+
+        body {
+            font-family: 'Nunito', sans-serif;
+        }
+
+        body {
+            background: lightgrey;
+            display: flex;
+            justify-content: center;
+        }
+
+
+        .page {
+            padding: 50px 80px;
+            margin: 50px;
+            background: white;
+            box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.6);
+            max-width: 800px;
+            min-width: 500px;
+        }
+
+        @media only screen and (max-width: 1200px) {
+            body {
+                background: lightgrey;
+                display: flex;
+                justify-content: center;
+            }
+
+            /*Tablets [601px -> 1200px]*/
+            .page {
+                padding: 50px 80px;
+                margin: 50px;
+                background: white;
+                box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.6);
+                max-width: 800px;
+                min-width: 500px;
+            }
+        }
+
+        @media only screen and (max-width: 600px) {
+            body {
+                background: lightgrey;
+                display: flex;
+                justify-content: center;
+            }
+
+            /*Big smartphones [426px -> 600px]*/
+            .page {
+                padding: 50px 80px;
+                margin: 50px;
+                background: white;
+                box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.6);
+                max-width: 800px;
+                min-width: 500px;
+            }
+        }
+
+        @media only screen and (max-width: 425px) {
+            body {
+                background: lightgrey;
+                display: flex;
+                justify-content: center;
+                width: 90% !important;
+            }
+
+            /*Small smartphones [325px -> 425px]*/
+            .page {
+                padding: 30px 40px;
+                margin: 50px;
+                background: white;
+                box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.6);
+                max-width: 90%;
+                min-width: 90%;
+            }
+        }
+
+        #terms-and-conditions {
+            font-size: 16px; // default
+
+            h1 {
+                font-size: 34px;
+            }
+
+            ol {
+                counter-reset: item;
+            }
+
+            li {
+                padding: 20px !important;
+                margin: 20px 0 !important;
+            }
+        }
+    </style>
+</head>
+
+<body class="antialiased  overflow-x-hidden">
+    <div class="page">
+        <div id="terms-and-conditions">
+            <h1>Privacy Policy</h1>
+            <ol>
+                <li>
+                    <b>Collection and Processing Data:</b>
+                    <ul>
+                        <li>
+                            We collect personal information such as name, email address, and shipping address when you
+                            place an order on our website. This information helps us to ship the item to you swiftly. It
+                            is mandatory to provide customer support and improve our products and services.
+                        </li>
+                    </ul>
+                </li>
+                <br />
+
+                <li>
+                    <b>Third-Party Disclosure:</b>
+                    <ul>
+                        <li>
+                            We do not sell, trade, or otherwise transfer your personal information to third parties
+                            under any circumstances, except to fulfill your order.
+
+                        </li>
+                    </ul>
+                </li>
+                <br />
+
+                <li>
+                    <b>Cookies:</b>
+                    <ul>
+                        <li>
+                            We use cookies to enhance your browsing experience and gather information about how you use
+                            our website.
+                        </li>
+                    </ul>
+                </li>
+                <br />
+
+                <li>
+                    <b>Contact Information:</b>
+                    <ul>
+                        <li>
+                            If you have any questions or concerns about our privacy practices, please contact us at <a
+                                href="mailto:lisahealthcare01@gmail.com">lisahealthcare01@gmail.com</a>
+                        </li>
+                    </ul>
+                </li>
+                <br />
+
+                <li>
+                    <b>Return Policy:</b>
+                    <ul>
+                        <li>
+                            Your wellbeing is important to us and we strive to maintain high standards of hygiene. Due
+                            to the intimate nature of Lisa’s menstrual hygiene products, once delivered, all products
+                            are non-returnable.
+                            However, in the unlikely event of damaged, defective, or different/wrong items being
+                            delivered to you, please write to us at <a
+                                href="mailto:lisahealthcare01@gmail.com">lisahealthcare01@gmail.com</a> with the details
+                            and
+                            supporting image(s)/video(s).
+                            <br />
+                            <br />
+                            We consider all such cases individually. Upon receipt of the grievance, we will conduct a
+                            full and thorough examination and notify you within a reasonable time. Please note that we
+                            accept order-related concerns (e.g., damaged, defective, only up to 5 days from the date of
+                            delivery).
+                        </li>
+                    </ul>
+                </li>
+                <br />
+
+                <li>
+                    <b>Cancellation Policy:</b>
+                    <ul>
+                        <li>
+                            Once an order is placed (inclusive of orders that are automatically placed under Auto-repeat
+                            Plans* soon), it can be canceled as long as the order has not been handed over to our
+                            delivery partner. While orders take about a day to be dispatched, we recommend that order
+                            cancellation requests be shared within 90 minutes of order confirmation.
+                            <br />
+                            <br />
+                            In case you would like to cancel your order within this timeframe, please contact us via our
+                            live chat, write to us at <a
+                            href="mailto:lisahealthcare01@gmail.com">lisahealthcare01@gmail.com</a>, or give us a call at <a
+                            href="tel:+91 74188 80025">+91 74188 80025</a>.
+                        </li>
+                    </ul>
+                </li>
+                <br />
+                <li>
+                    <b>Refunds Policy: </b>
+                    <ul>
+                        <li>
+                            Upon cancellation of prepaid orders, a refund of the order amount will be initiated in 24-48 hours and it shall be credited to the original payment on or before 3-5 working days.
+                        </li>
+                    </ul>
+                </li>
+                <br />
+            </ol>
+            
+        </div><!--  end #terms-and-conditions  -->
+    </div><!--  end .page  -->
+</body>
+
+</html>
